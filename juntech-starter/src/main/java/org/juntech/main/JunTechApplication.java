d8e88de9359a8c9c84b8c074e70bf461f6b6c4b7
jun <271922405@qq.com>
2024-03-24 16:23:33 +0800

    SpringBoot 环境配置理解（ dev、test、preview、prod）

--- juntech-starter/src/main/java/org/juntech/main/JunTechApplication.java
+++ juntech-starter/src/main/java/org/juntech/main/JunTechApplication.java
@@ -2,11 +2,20 @@ package org.juntech.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
 
 @SpringBootApplication(scanBasePackages = {"org.juntech"})
 public class JunTechApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JunTechApplication.class,args);
+        ConfigurableApplicationContext application =  SpringApplication.run(JunTechApplication.class,args);
+        // 打印启动信息
+        Environment env = application.getEnvironment();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        System.out.println("Application Boot is running port: "+port+", path："+path);
     }
 }
